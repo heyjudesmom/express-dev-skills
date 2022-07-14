2f7e498ab5f20b6d3766e752f1a9f9a4f08da0fd
heyjudesmom <brianna.g.mccarthy@gmail.com>
2022-07-13 17:07:40 -0700

    Add link and Delete button

--- routes/skills.js
+++ routes/skills.js
@@ -8,6 +8,12 @@ var skillsCtrl = require('../controllers/skills');
 
  // GET /skills
  router.get('/', skillsCtrl.index);
-module.exports = router;
-// GET /skills/:id
-router.get('/:id', skillsCtrl.show);
+ //GET /skills/new
+ router.get('/new', skillsCtrl.new)
+ // GET /skills/:id
+ router.get('/:id', skillsCtrl.show);
+ //POST /
+ router.post('/', skillsCtrl.create);
+ //DELETE
+ router.delete('/:id', skillsCtrl.delete);
+ module.exports = router;
